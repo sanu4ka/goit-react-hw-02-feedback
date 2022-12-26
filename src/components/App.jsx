867066a587a8f4b5e3680b8b75ae37f7a25bb3c2
sanu4ka <sanu4ka1989@gmail.com>
2022-12-26 23:26:41 +0200

    finish task

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,9 @@
 import { Component } from 'react';
+import css from './App.module.css';
+import Statictics from './Statistics';
+import FeedbackOptions from './FeedbackOptions';
+import Section from './Section';
+import Notification from './Notification';
 
 const INITIAL_STATE = {
   good: 0,
@@ -7,43 +12,53 @@ const INITIAL_STATE = {
 };
 
 export class App extends Component {
-  constructor() {
-    super();
-    this.state = { ...INITIAL_STATE };
-  }
+  state = { ...INITIAL_STATE };
 
-  onLeaveFeedback = () => {
-    this.setState(prevState => {
-      return { good: prevState.good + 1 };
-    });
+  onLeaveFeedback = option => {
+    this.setState(prevState => ({ [option]: prevState[option] + 1 }));
+  };
+
+  countTotalFeedback = array => {
+    let sum = 0;
+    for (var i = 0; i < array.length; i++) {
+      sum += array[i];
+    }
+    return sum;
+  };
+
+  countPositiveFeedbackPercentage = () => {
+    const total = this.countTotalFeedback(Object.values(this.state));
+    const { good } = this.state;
+
+    return Math.round((good / total) * 100) || 0;
   };
 
   render() {
+    let options = Object.keys(this.state);
     const { good, neutral, bad } = this.state;
+    let total = this.countTotalFeedback(Object.values(this.state));
+    let positivePercentage = this.countPositiveFeedbackPercentage();
     return (
-      <div>
-        <h2>Please leave feedback</h2>
-        <button type="button" onClick={this.onLeaveFeedback}>
-          Good
-        </button>
-        <button type="button" onClick={this.onLeaveFeedback}>
-          Neutral
-        </button>
-        <button type="button" onClick={this.onLeaveFeedback}>
-          Bad
-        </button>
-        <h3>Statistics</h3>
-        <ul>
-          <li>
-            <p>Good: {good}</p>
-          </li>
-          <li>
-            <p>Neutral: {neutral}</p>
-          </li>
-          <li>
-            <p>Bad: {bad}</p>
-          </li>
-        </ul>
+      <div className={css.mainModule}>
+        <Section title="Please leave feedback">
+          <FeedbackOptions
+            options={options}
+            onLeaveFeedback={this.onLeaveFeedback}
+          />
+        </Section>
+        <Section title="Statistics">
+          {total ? (
+            <Statictics
+              good={good}
+              neutral={neutral}
+              bad={bad}
+              total={total}
+              positivePercentage={positivePercentage}
+            />
+          ) : (
+            <Notification message="There is no feedback" />
+          )}
+        </Section>
       </div>
     );
   }
